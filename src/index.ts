02f98e415caa183997d494c1e17e7954c278c0fa
GabrielGalindo1 <gabrieldelimagalindo.gdlg@gmail.com>
2022-03-17 07:49:52 -0300

    Foi usado sequelize para criação e manipulação do banco de dados, migrations com o sequelize cli para exportar as tabelas para o banco e o readline para inserção dos dados

--- src/index.ts
+++ src/index.ts
@@ -2,42 +2,78 @@
  * Required External Modules
  */
 
-import * as dotenv from "dotenv";
-import express from "express";
-import cors from "cors";
-import helmet from "helmet";
+ import * as dotenv from "dotenv";
+ import express from "express";
+ import cors from "cors";
+ import helmet from "helmet";
+ import { read } from "fs";
+ 
+ const readline = require('readline-sync');
+ const db = require('../models/index.js');
+ 
+ 
+ dotenv.config();
+ 
+ /**
+  * App Variables
+  */
+ 
+ if (!process.env.PORT) {
+	 process.exit(1);
+ }
+ 
+ const PORT: number = parseInt(process.env.PORT as string, 10);
+ 
+ const app = express();
+ 
+ /**
+  *  App Configuration
+  */
+ 
+ app.use(helmet());
+ app.use(cors());
+ app.use(express.json());
+ 
+ /**
+  * Server Activation
+  */
+ 
+ app.listen(PORT, async () => {
+	 console.log(`Listening on port ${PORT}`);
+ 
+ 
+	 // CÓDIGO PARA ATENDER OS REQUERIMENTOS
+	 // R01, R02, R03, R04, R05
+		 const qtd = parseInt(readline.question('Qual a quantidade de alunos ?\n'));
+ 
+		 for(let i=0 ; i < qtd ; i++){
+ 
+			 const id = parseInt(readline.question('Qual o id do aluno ?\n'));
+ 
+			 const name = readline.question('Qual o nome do aluno ?\n');
+ 
+			 const grade = parseInt(readline.question('Qual a nota do aluno ?\n'));
+ 
+			 const email = readline.question('Qual o email do aluno ?\n')
 
-dotenv.config();
+			 await db.Aluno.create({id: id, name: name, grade: grade, email: email})
+			 			
+		 }
 
-/**
- * App Variables
- */
-
-if (!process.env.PORT) {
-	process.exit(1);
-}
-
-const PORT: number = parseInt(process.env.PORT as string, 10);
-
-const app = express();
-
-/**
- *  App Configuration
- */
-
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-
-/**
- * Server Activation
- */
-
-app.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}`);
-
-
-	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
-	// R01, R02, R03, R04, R05
+		 /**Consulta simples findAll
+		const users = await db.Aluno.findAll();
+		console.log("All users:", JSON.stringify(users, null));
 	
+		console.log(users);
+
+		//Update
+		const item = await db.Aluno.findByPk(1);
+		item.name = "";
+		item.grade = '';
+		item.save();
+	  
+		//Delete
+		await db.Aluno.findByPk(6);
+		item.destroy();**/
 });
+ 
